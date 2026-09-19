949cc8456a0620a292003ad9a21551a49a89881e
parham112289 <parhamsadr.s89@gmail.com>
2026-09-19 15:50:39 +0330

    AK1_13

--- phone_app/android/app/src/main/kotlin/com/ak1/phone/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ak1.phone
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/android/app/src/main/kotlin/com/example/robot_ak1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.robot_ak1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
